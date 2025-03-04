bce40a1e97837061c373a77f8c755e336d3d97ee
shinwoo park <turbospace@naver.com>
2025-03-04 22:05:37 +0900

    firebase 설치 및 초기화화

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDojPBD0aN-s8YnvP7b6spC-yPP866G2VI",
+  authDomain: "nwritter-reloaded-cc101.firebaseapp.com",
+  projectId: "nwritter-reloaded-cc101",
+  storageBucket: "nwritter-reloaded-cc101.firebasestorage.app",
+  messagingSenderId: "439386356426",
+  appId: "1:439386356426:web:b73c08f3d22d965bb5d233"
+};
+
+const app = initializeApp(firebaseConfig);
