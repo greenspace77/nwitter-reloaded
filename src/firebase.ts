77d46758f70b36bc3552ab9f4457d6731a5c320a
shinwoo park <turbospace@naver.com>
2025-03-05 00:04:03 +0900

    firebase auth 추가 1

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDojPBD0aN-s8YnvP7b6spC-yPP866G2VI",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
