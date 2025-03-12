624da5145a21110801073d910e36ee1122c8bb94
greenspace77 <greenspace@sk.com>
2025-03-12 10:49:52 +0900

    layout 클래스에 logout 로직 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,18 +1,6 @@
-import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
 export default function Home() {
-  const navigate = useNavigate();
-
-  const logOut = async () => {
-    console.log("melong");
-    await auth.signOut();
-    navigate("/login");
-  };
-
   return (
     <h1>
-      <button onClick={logOut}>Log out</button>
     </h1>
   );
 }
